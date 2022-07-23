80f5d8f34ab51655e1860ce9502ac7428ddc75c0
dgoyanez <106639113+dgoyanez@users.noreply.github.com>
2022-07-23 17:21:00 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO DOJO!</h1>
+      <h2>Things I need to do:</h2>
+      <p>* Learn React</p>
+      <p>* Climb Mt. Everest</p>
+      <p>* Run a Marathon</p>
+      <p>* Feed the Dogs</p>
+    </div>
+  );
+}
+
+export default App;
